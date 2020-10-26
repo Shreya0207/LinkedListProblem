e7366516ac86cc906154e7dc82ea0ddf2dd16fd5
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-26 20:47:55 +0530

    [Shreya]Add: Inserting the test method

--- LinkedListTest/UnitTest1.cs
+++ LinkedListTest/UnitTest1.cs
@@ -15,5 +15,15 @@ namespace LinkedListTest
             list.Add(56);
             Assert.IsTrue(list.Search(30));
         }
+        [TestMethod]
+        public void TestInsert()
+        {
+            LinkedList list = new LinkedList();
+            list.Append(56);
+            list.Append(30);
+            list.Append(70);
+            list.Insert(3, 40);
+            Assert.IsTrue(list.Search(40));
+        }
     }
 }
