e86b88c8882bce8f5f97d205e6ac41cf76f3dd51
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-26 20:07:41 +0530

    [Shreya]Add: Appending 30 and 70 to 56.

--- LinkedListProgram/Program.cs
+++ LinkedListProgram/Program.cs
@@ -8,9 +8,9 @@ namespace LinkedListProgram
         {
             Console.WriteLine("Welcome to Linked List Progrm");
             LinkedList list = new LinkedList();
-            list.Add(70);
-            list.Add(30);
-            list.Add(56);
+            list.Append(56);
+            list.Append(30);
+            list.Append(70);
             list.Display();
         }
     }
