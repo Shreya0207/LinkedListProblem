040700185a48c7da1591ceb2b4924d12454acaf8
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-26 19:53:35 +0530

    [Shreya]Add: Creation of the linked list.

---
+++ LinkedListProgram/LinkedList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListProgram
+{
+    class LinkedList
+    {
+        internal Node head;
+        public LinkedList()
+        {
+            this.head = null;
+        }
+    }
+}
---
+++ LinkedListProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LinkedListProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Progrm");
+            Node head = new Node(56);
+            head.next = new Node(30);
+            head.next.next = new Node(70);
+        }
+    }
+}
